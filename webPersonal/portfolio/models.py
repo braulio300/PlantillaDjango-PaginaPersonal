f9cb05a774ecd84690fb037faeb711d1a4d3dfd8
braulio300 <lucas.zosst@gmail.com>
2020-11-05 18:05:19 -0300

    Plantilla Django

---
+++ webPersonal/portfolio/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title       = models.CharField(max_length=200, verbose_name="Titulo")
+    description = models.TextField(verbose_name="Descripcion")
+    image       = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link        =models.URLField(verbose_name="Dirección Web", null=True, blank=True)
+    created     = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated     = models.DateTimeField(auto_now=True, verbose_name="Fecha de modificación")
+    
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering =["-created"]
+    
+    def __str__(self):
+        return self.title
